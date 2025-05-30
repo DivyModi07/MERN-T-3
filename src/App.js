a908ead0f5d931e090e5fb5f3bdcdad020f02c9d
DivyModi07 <divymodi1235@gmail.com>
2025-05-30 13:25:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,48 @@ import './App.css';
 // import Task1 from './Task1'
 import Task2 from './Task2'
 import {Add,Sub} from './Task2'
+import {Task4,Task5} from './Task4'
 
+import Task6 from './Task6'
+
+import Task7 from './Task7'
+
+import Task8 from './Task8'
+
+import Task9 from './Task9'
 
 
 
 function App() {
+  const item = 'mobile'
+  const arr = {name:'abc',uni:'lju'}
+  const obj = [{name:'mobile',img:'logo192.png',price:'15000',disc:'Hello Mi note 11'},{name:'mobile',img:'logo192.png',price:'20000',disc:'Hello Mi note 12'},{name:'mobile',img:'logo192.png',price:'35000',disc:'Hello Mi note 12 Pro'}]
+
+
   return (
     <div>
-      <h1>Hello from app.js</h1>
+      {/* <h1>Hello from app.js</h1> */}
       {/* <Pr1 />
       <Task1 /> */}
-      <Add />
-      <Sub />
+      {/* <Add />
+      <Sub /> */}
       {/* <Task2 />       */}
+
+      {/* <Task4 name='Laptop' price = '50000'></Task4>
+      <Task4 name='TV' price = '35000'></Task4>
+      <Task4 name={item} price = '25000'></Task4>
+      <Task5 data={arr}></Task5> */}
+
+      {/* <Task6 img='logo192.png' name='Mobile' price='15000' disc='Hello Mi note 11'></Task6>
+      <Task6 img='logo192.png' name='Mobile' price='20000' disc='Hello Mi note 12'></Task6>
+      <Task6 img='logo192.png' name='Mobile' price='15000' disc='Hello Mi note 12 Pro'></Task6> */}
+        
+      {/* <Task7/> */}
+
+      {/* <Task8 obj={obj}/> */}
+
+      <Task9/>
+
     </div>
   );
 }
